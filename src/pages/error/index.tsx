4b41ea02098fc56afa0d0ed988323e978303c302
Daloniil <62095873+Daloniil@users.noreply.github.com>
2022-06-16 21:18:13 +0300

    fix error page

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useNotification } from "../../hooks/useNotification";
+import { NotificationKeys } from "../../services/localKey";
+
 export const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const { addNotification } = useNotification();
+
+  useEffect(() => {
+    navigate("/");
+    addNotification("Error 404", NotificationKeys.ERROR);
+  }, []);
+
   return <div>Error 404</div>;
 };
